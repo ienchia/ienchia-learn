241be4b5754734c0dba31a8849a2b158b3cd8523
ienchia <ienchia@users.noreply.github.com>
2015-02-06 20:55:06 +0700

    add source folder

---
+++ src/com/ienchia/ienchiaLearnJava/LearnJava.java
@@ -0,0 +1,18 @@
+package com.ienchia.ienchiaLearnJava;
+
+import java.io.PrintStream;
+
+public class LearnJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Set console output to printStream
+		PrintStream printStream = System.out;
+		
+		// Output greetings to printStream
+		printStream.printf("Hello, %s!", "World");
+
+	}
+
+}
